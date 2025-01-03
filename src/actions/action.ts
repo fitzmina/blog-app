762760c7ab1beb23b5e368d4ad1dd64dfab21b7a
Fitz Mina <fitzgmina@gmail.com>
2025-01-03 19:20:56 +0800

    4

--- src/actions/action.ts
+++ src/actions/action.ts
@@ -9,7 +9,7 @@ export async function createPost(formData: FormData) {
   const { isAuthenticated } = getKindeServerSession();
 
   if (!(await isAuthenticated())) {
-    redirect("/api/auth/login?post_login_redirect_url=/create");
+    redirect("/api/auth/login");
   }
 
   const title = formData.get("title") as string;
